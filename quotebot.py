03749e1cb0c2d1f2a7ca60a8b9e10d4913319087
N <znkonscratch@gmail.com>
2015-03-29 16:08:12 -0700

    Fixed another bug.

--- quotebot.py
+++ quotebot.py
@@ -98,9 +98,7 @@ USEDB = True
 
 if USEDB == True and os.path.isfile(os.path.expanduser("~") +'/.quotebot_database.p'):
     dbLoad = pickle.load(open(os.path.expanduser("~") +'/.quotebot_database.p','rb'))
-    if dbLoad['version'] == [0,2,0]:
-        quotes = dbLoad['quotes']
-    if dbLoad['version'] == [0,3,0]:
+    if dbLoad['version'] == [0,1,0,"d"]:
         quotes = dbLoad['quotes']
     else:
         print "This database was created with an old or unknown version of quotebot. Please use the newest version (or correct fork) and try again. If this is not possible, move or delete the file '~/.quotebot_database.p' and re-run quotebot. A new database will be created automatically."
